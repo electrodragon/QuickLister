f3fea86aaa9cd4d2c027a47b2abf13aad34cf9da
electrodragon <attaullahkhanlodhi@gmail.com>
2020-01-15 19:07:15 +0500

    Created FireMethod.java File
    Created to Hold Some Useful Methods for MainActivity.java

---
+++ app/src/main/java/com/drunk_assassins/quicklister/FireMethod.java
@@ -0,0 +1,25 @@
+package com.drunk_assassins.quicklister;
+
+public class FireMethod {
+
+    public static String[][] fetchArrayFromDB(int position) {
+        String[][] tempArray;
+
+        if (position == 0) {
+            tempArray = DB.MOBILE_APPLICATION_DEVELOPMENT;
+        } else if (position == 1) {
+            tempArray = DB.CCTV;
+        } else if (position == 2) {
+            tempArray = DB.GRAPHICS;
+        } else if (position == 3) {
+            tempArray = DB.NETWORKING;
+        } else {
+            tempArray = DB.SAFETY;
+        }
+
+        return tempArray;
+    }
+
+    
+
+}
