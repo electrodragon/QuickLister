b986cc273bf150e472a4ab7986fec4a94435cc85
electrodragon <attaullahkhanlodhi@gmail.com>
2020-01-15 17:53:50 +0500

    Added MOBILE-APPLICATION-DEVELOPMENT data in  DB.JAVA

---
+++ app/src/main/java/com/drunk_assassins/quicklister/DB.java
@@ -0,0 +1,52 @@
+package com.drunk_assassins.quicklister;
+
+public class DB {
+
+    public static String[][] MOBILE_APPLICATION_DEVELOPMENT = {
+            {"Roll no: 01", "Name: Mursal Khosa", "Email: Khosa10@gmail.com", "Phone no: (767) 361-8612", "Gender: Male"},
+            {"Roll no: 02", "Name: Daena Tirmizi", "Email: Tirmizi20@gmail.com", "Phone no: (342) 267-8996", "Gender: Female"},
+            {"Roll no: 03", "Name: Shaida Chatha", "Email: Chatha30@gmail.com", "Phone no: (945) 640-1932", "Gender: Female"},
+            {"Roll no: 04", "Name: Hammad Qureshi", "Email: Qureshi40@gmail.com", "Phone no: (294) 408-5539", "Gender: Male"},
+            {"Roll no: 05", "Name: Almirah Askari", "Email: Askari50@gmail.com", "Phone no: (308) 907-9653", "Gender: Female"},
+            {"Roll no: 06", "Name: Abbar Chishti", "Email: Chishti60@gmail.com", "Phone no: (909) 800-6228", "Gender: Male"},
+            {"Roll no: 07", "Name: Ahlaam Cheema", "Email: Cheema70@gmail.com", "Phone no: (240) 899-7025", "Gender: Male"},
+            {"Roll no: 08", "Name: Ayad Tiwana", "Email: Tiwana80@gmail.com", "Phone no: (590) 568-8968", "Gender: Male"},
+            {"Roll no: 09", "Name: Yanni Zimri", "Email: Zimri90@gmail.com", "Phone no: (454) 644-7336", "Gender: Female"},
+            {"Roll no: 10", "Name: Aeesha Jaspal", "Email: Jaspal01@gmail.com", "Phone no: (219) 708-1144", "Gender: Female"},
+            {"Roll no: 11", "Name: Sumbul Saifi", "Email: Sumbul11@gmail.com", "Phone no: (678) 535-7584", "Gender: Female"},
+            {"Roll no: 12", "Name: Aneesah Virk", "Email: Aneesah21@gmail.com", "Phone no: (269) 337-2951", "Gender: Female"},
+            {"Roll no: 13", "Name: Niyaz Davi", "Email: Davi31@gmail.com", "Phone no: (207) 415-5211", "Gender: Male"},
+            {"Roll no: 14", "Name: Beenish Hashmi", "Email: Hashmi41@gmail.com", "Phone no: (363) 534-3477", "Gender: Female"},
+            {"Roll no: 15", "Name: Hameed Gul", "Email: Gul51@gmail.com", "Phone no: (429) 793-5449", "Gender: Male"},
+            {"Roll no: 16", "Name: Horiya Wani", "Email: Wani61@gmail.com", "Phone no: (483) 286-9915", "Gender: Female"},
+            {"Roll no: 17", "Name: Hedaya Mughal", "Email: Mughal71@gmail.com", "Phone no: (495) 691-1329", "Gender: Female"},
+            {"Roll no: 18", "Name: Hunain Bajwa", "Email: Bajwa81@gmail.com", "Phone no: (486) 680-4754", "Gender: Male"},
+            {"Roll no: 19", "Name: Hamil Sarpara", "Email: Sarpara91@gmail.com", "Phone no: (225) 928-2477", "Gender: Male"},
+            {"Roll no: 20", "Name: Hajar Niazi", "Email: Niazi02@gmail.com", "Phone no: (300) 790-2622", "Gender: Male"},
+            {"Roll no: 21", "Name: Atif Chauhan", "Email: Chauhan12@gmail.com", "Phone no: (722) 861-6072", "Gender: Male"},
+            {"Roll no: 22", "Name: Zari Mengal", "Email: Mengal22@gmail.com", "Phone no: (342) 291-4665", "Gender: Female"},
+            {"Roll no: 23", "Name: Nitasha Mazari", "Email: Mazari32@gmail.com", "Phone no: (630) 431-6687", "Gender: Male"},
+            {"Roll no: 24", "Name: Hameeda Jafari", "Email: Jafari42@gmail.com", "Phone no: (211) 352-4762", "Gender: Female"},
+            {"Roll no: 25", "Name: Sanan Jamshidi", "Email: Jamshidi52@gmail.com", "Phone no: (242) 352-8937", "Gender: Female"},
+            {"Roll no: 26", "Name: Naila Sangh", "Email: Sangh62@gmail.com", "Phone no: (341) 967-9120", "Gender: Female"},
+            {"Roll no: 27", "Name: Mehwish Malik", "Email: Malik72@gmail.com", "Phone no: (514) 483-4874", "Gender: Female"},
+    };
+
+
+    public static String[][] CCTV = {
+
+    };
+
+    public static String[][] GRAPHICS = {
+
+    };
+
+    public static String[][] NETWORKING = {
+
+    };
+
+    public static String[][] SAFETY = {
+
+    };
+
+}
